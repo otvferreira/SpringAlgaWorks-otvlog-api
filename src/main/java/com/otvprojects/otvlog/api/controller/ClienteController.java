0aea2e32a4ffd91f9124accc0b610b27669ca4e0
Otavio <ottvferreira@gmail.com>
2022-07-23 22:26:53 -0300

    Metodos @GetMapping, @PostMapping e @PutMapping

--- src/main/java/com/otvprojects/otvlog/api/controller/ClienteController.java
+++ src/main/java/com/otvprojects/otvlog/api/controller/ClienteController.java
@@ -2,12 +2,23 @@ package com.otvprojects.otvlog.api.controller;
 
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.otvprojects.otvlog.domain.model.Cliente;
@@ -15,15 +26,50 @@ import com.otvprojects.otvlog.domain.repository.ClienteRepository;
 
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 
+	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId)
+			.map(cliente -> ResponseEntity.ok(cliente))
+			.orElse(ResponseEntity.notFound().build());
+		
+//		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+//		
+//		if (cliente.isPresent()) {
+//		return ResponseEntity.ok(cliente.get());
+//		}
+//		
+//		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+		@RequestBody Cliente cliente){
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
 	}
 }
--- src/main/java/com/otvprojects/otvlog/domain/repository/ClienteRepository.java
+++ src/main/java/com/otvprojects/otvlog/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.otvprojects.otvlog.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.otvprojects.otvlog.domain.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
 	
 }
