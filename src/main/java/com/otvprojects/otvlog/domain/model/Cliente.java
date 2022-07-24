59500d1d049520400ea00cf6a6aa1870d14618b8
Otavio <ottvferreira@gmail.com>
2022-07-23 21:00:51 -0300

    Equals and Hash Code

--- src/main/java/com/otvprojects/otvlog/domain/model/Cliente.java
+++ src/main/java/com/otvprojects/otvlog/domain/model/Cliente.java
@@ -1,15 +1,34 @@
 package com.otvprojects.otvlog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 
 @Getter
 @Setter
+//@Entity
+@Table(name = "cliente")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
+	
 	private String email;
+	
+	@Column(name = "telefone1")
 	private String telefone;
+	
+	
 }
