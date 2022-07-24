9a6ca7d761a1b163d0f2dbf1372970ec7089ba35
Otavio <ottvferreira@gmail.com>
2022-07-23 23:06:20 -0300

    Adiciona retorno com tratamento

---
+++ src/main/java/com/otvprojects/otvlog/api/exceptionhandler/Problema.java
@@ -0,0 +1,14 @@
+package com.otvprojects.otvlog.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Problema {
+	private int status;
+	private LocalDateTime dataHora;
+	private String titulo;
+}
