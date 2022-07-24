4ad570c1e9d351d07cdd808b2e3a639ea7e40c59
Otavio <ottvferreira@gmail.com>
2022-07-23 21:31:48 -0300

    Adiciona @Autowired

--- src/main/java/com/otvprojects/otvlog/api/controller/ClienteController.java
+++ src/main/java/com/otvprojects/otvlog/api/controller/ClienteController.java
@@ -6,22 +6,24 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.otvprojects.otvlog.domain.model.Cliente;
+import com.otvprojects.otvlog.domain.repository.ClienteRepository;
 
 
 @RestController
 public class ClienteController {
 
-	@PersistenceContext
-	private EntityManager manager;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return clienteRepository.findAll();
 
 	}
 }
