80441413e5bf8a7be15326af21410a5bdea612c2
Otavio <ottvferreira@gmail.com>
2022-07-23 23:41:38 -0300

    Editando validações

--- src/main/java/com/otvprojects/otvlog/api/exceptionhandler/Problema.java
+++ src/main/java/com/otvprojects/otvlog/api/exceptionhandler/Problema.java
@@ -1,7 +1,9 @@
 package com.otvprojects.otvlog.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,4 +13,13 @@ public class Problema {
 	private int status;
 	private LocalDateTime dataHora;
 	private String titulo;
+	private List<Campo> campos;
+	
+	@AllArgsConstructor
+	@Getter
+	public static class Campo{
+		
+		private String nome;
+		private String mensagem;
+	}
 }
